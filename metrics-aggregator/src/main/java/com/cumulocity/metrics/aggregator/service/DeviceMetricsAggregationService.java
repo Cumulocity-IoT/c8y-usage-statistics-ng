3294cd53719971fe3ca8772675efce8d7ac1e682
Marco Stoffel <marco.stoffel@cumulocity.com>
2025-03-10 13:38:39 +0100

    only fetch when older than 24 hours

--- metrics-aggregator/src/main/java/com/cumulocity/metrics/aggregator/service/DeviceMetricsAggregationService.java
+++ metrics-aggregator/src/main/java/com/cumulocity/metrics/aggregator/service/DeviceMetricsAggregationService.java
@@ -221,7 +221,7 @@ public class DeviceMetricsAggregationService {
 		Instant now = Instant.now();
 		long hours = ChronoUnit.HOURS.between(this.dailyDeviceStatisticsAggregationLastRun, now );
 		// fetch only when older than 12 hours
-		if (omitCache || hours > 12 )	 {
+		if (omitCache || hours > 24 )	 {
 			log.info("Getting Daily stats.");
 			createDailyDeviceStatistics();
 		}
