af807fdf80b5a7ceb6f08db6d4e7ee921dece8ca
Marco Stoffel <marco.stoffel@cumulocity.com>
2025-03-12 12:26:49 +0100

    remove imports

--- metrics-aggregator/src/main/java/com/cumulocity/metrics/aggregator/service/BootstrapService.java
+++ metrics-aggregator/src/main/java/com/cumulocity/metrics/aggregator/service/BootstrapService.java
@@ -1,19 +1,11 @@
 package com.cumulocity.metrics.aggregator.service;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
-import com.cumulocity.metrics.aggregator.service.StartupUtils;
-
-import com.cumulocity.metrics.aggregator.model.device.DeviceStatisticsAggregation;
 import com.cumulocity.microservice.subscription.model.MicroserviceSubscriptionAddedEvent;
-import com.cumulocity.microservice.subscription.service.MicroserviceSubscriptionsService;
-import com.cumulocity.sdk.client.RestConnector;
 
 
 
@@ -31,10 +23,6 @@ import com.cumulocity.sdk.client.RestConnector;
 public class BootstrapService {
 
     @Autowired
-    RestConnector restConnector;
-    
-    
-    @Autowired
     DeviceMetricsAggregationService deviceMetricsAggregationService;
  
 
@@ -42,19 +30,6 @@ public class BootstrapService {
     StartupUtils utils;
 
     private static final Logger log = LoggerFactory.getLogger(BootstrapService.class);
-
-    private String currentTenant = ""; 
-    private List<String> tenantList;
-
-
-
-    public String getCurrentTenant() {
-        return currentTenant;
-    }
-
-    public List<String> getTenantList() {
-        return tenantList;
-    }
 
 
 
@@ -65,7 +40,6 @@ public class BootstrapService {
 
         utils.getTenants(currentTenant);
         deviceMetricsAggregationService.createDailyDeviceStatistics();
-        //deviceMetricsAggregationService.createDailyDeviceStatistics();
 	}
 
 }
